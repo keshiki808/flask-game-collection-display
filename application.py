7035d2c2c3a40f9b291beed2861977c0a67168ab
keshiki808 <keshiki808@gmail.com>
2021-11-17 00:49:14 -0330

    Rearranged html files, began template work

--- application.py
+++ application.py
@@ -1,2 +1,13 @@
+import sqlite3
+from flask import Flask, render_template, redirect, request, abort
+import os
 
+app = Flask(__name__)
+app.config['UPLOAD_PATH'] = 'static/images'
 conn = sqlite3.connect("games.db", check_same_thread=False)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
