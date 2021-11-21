471d1b4c1e985d77319d4bb048a0c456ba1581b6
keshiki808 <keshiki808@gmail.com>
2021-11-20 22:46:57 -0330

    Template and html page updtes and form implementation

--- application.py
+++ application.py
@@ -11,3 +11,12 @@ conn = sqlite3.connect("games.db", check_same_thread=False)
 def index():
     return render_template("index.html")
 
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/collection", methods=["POST", "GET"])
+def collection():
+    return render_template("collection.html", game_name=request.form.get("game_name"))
+
+
