8c6ce8405c5d48bcf0846c9267ca44641a705b49
keshiki808 <keshiki808@gmail.com>
2021-11-24 23:31:19 -0330

    Added success/failure pages and modified templates

--- application.py
+++ application.py
@@ -6,17 +6,25 @@ app = Flask(__name__)
 app.config['UPLOAD_PATH'] = 'static/images'
 conn = sqlite3.connect("games.db", check_same_thread=False)
 
+# SCORES =["1","2","3","4","5"]
+SCORES =[1,2,3,4,5]
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+
 @app.route("/upload")
 def upload():
-    return render_template("upload.html")
+    return render_template("upload.html",scores=SCORES)
 
-@app.route("/collection", methods=["POST", "GET"])
+
+@app.route("/submitted", methods=["POST"])
 def collection():
-    return render_template("collection.html", game_name=request.form.get("game_name"))
+    if not request.form.get("game_name") or not request.form.get("score") not in SCORES:
+        return render_template("unsuccessful.html")
+    return render_template("submitted.html", game_name=request.form.get("game_name"))
 
 
